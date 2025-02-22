6bd9f8f94396e8791072274a20bca6a81f795f34
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2025-02-22 12:36:00 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
 const app = require('./app');
+const connectDB = require('./config/DBconfig');
+const seedDatabase = require('./config/seedDatabase');
 
 const port = process.env.PORT;
+
+connectDB().then(async () => {
+    await seedDatabase();
+});
+
 // start server
 app.listen(port, () => {
-    console.log('Yeah I run');
+    console.log(`Server is running on port ${port}`);
 });
